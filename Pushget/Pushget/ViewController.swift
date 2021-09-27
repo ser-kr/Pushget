16ae851e907bb8579ccdd4bee017b5cc46f3e55b
ser-kr <serhii.kalatur@gmail.com>
2021-09-27 10:42:21 +0300

    Cleanup

--- Pushget/Pushget/ViewController.swift
+++ Pushget/Pushget/ViewController.swift
@@ -9,25 +9,16 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    @IBAction func get(_ sender: Any) {
-        AF.request("http://numbersapi.com/random/year").responseString { response in
-                    debugPrint("Response: \(response)")
-                    if response.error == nil {
-                        self.label.text = response.value
-                    } else {
-                        self.label.text = "Error"
-                    }
-                }
     
+    @IBOutlet weak var label: UILabel!
+    @IBAction func getFact(_ sender: Any) {
+        AF.request("http://numbersapi.com/random/year").responseString { response in
+            if response.error == nil {
+                self.label.text = response.value
+            } else {
+                self.label.text = "Error \(String(reflecting: response.error))"
+            }
+        }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
- 
-    }
-
-
 }
 
